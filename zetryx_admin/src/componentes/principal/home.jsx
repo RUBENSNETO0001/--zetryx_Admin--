47b1e2b68f5e58c9cfe1c99de3848cdf7b4fbac9
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2026-05-18 14:48:41 -0500

    criação e configuração do sistema segundario

---
+++ zetryx_admin/src/componentes/principal/home.jsx
@@ -0,0 +1,11 @@
+import Painel from './layer_painel.jsx';
+
+const Home = () => {
+    return (
+        <>
+            <Painel />
+        </>
+    )
+}
+
+export default Home;
---
+++ zetryx_admin/src/componentes/principal/layer_painel.jsx
@@ -0,0 +1,9 @@
+const Painel = () => {
+    return (
+        <>
+            <h1>Painel principal</h1>
+        </>
+    )
+}
+
+export default Painel;
